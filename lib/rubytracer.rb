f498e9c999d34fb178b615994746e904027ebf59
Wim Looman <ghostunderscore@gmail.com>
2011-01-11 21:21:59 +1300

    Adding requires

--- lib/rubytracer.rb
+++ lib/rubytracer.rb
@@ -1,3 +1,15 @@
+%w{
+  rubytracer/scene
+  rubytracer/view
+  rubytracer/camera
+  rubytracer/colour
+  rubytracer/material
+  rubytracer/shapes/sphere
+  rubytracer/lights/ambient
+}.each do |file|
+  require file
+end
+
 module RubyTracer
 
-end
+end 
