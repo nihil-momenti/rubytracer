d64d55b664c10e17f85d9729d7e12d0c9183d491
Wim Looman <ghostunderscore@gmail.com>
2011-01-20 23:57:23 +1300

    Moving all the cameras functionality to view

--- lib/rubytracer/camera.rb
+++ lib/rubytracer/camera.rb
@@ -5,18 +5,6 @@ module Rubytracer
       @scene = scene
     end
 
-    def colour_of_pixel(row, col)
-      ray = @view.eye_ray(row, col)
-      obj, t = @scene.intersect(ray)
-      if t == Float::INFINITY
-        Colour.new(0.6,0.6,0.6)
-      else
-        pos = ray.pos(t)
-        normal = obj.normal(pos)
-        obj.material.lit_colour(@scene, normal, -ray.dir, pos)
-      end
-    end
-
     def to_hash
       { :view => @view.to_hash, :scene => @scene.to_hash }
     end
--- lib/rubytracer/view.rb
+++ lib/rubytracer/view.rb
@@ -2,15 +2,20 @@ require 'geom3d'
 
 module Rubytracer
   class View
-    def initialize(point, multi, y_vector, x_vector, top_left)
+    attr_reader :height, :width
+
+    def initialize(point, multi, y_vector, x_vector, top_left, height, width)
       @point = point
       @multi = multi
       @y_vector = y_vector
       @x_vector = x_vector
       @top_left = top_left
+      @height = height
+      @width = width
+      @scene = scene
     end
 
-    def self.create(view_point, look_at_point, view_up, h_fov, height, width, multi)
+    def self.create(view_point, look_at_point, view_up, h_fov, height, width, multi, scene)
       view_direction = (look_at_point - view_point).unit
       view_up = view_up - view_direction * view_up.dot(view_direction)
       spacing = 2 * Math.tan(h_fov * Math::PI / 360) / width
@@ -20,7 +25,7 @@ module Rubytracer
       y_vector = -spacing * view_up.unit
       x_vector = spacing * view_up.unit.cross(view_direction.unit).unit
       top_left = (view_point + view_direction) - (y_vector * height / 2  + x_vector * width / 2)
-      View.new(point, multi, y_vector, x_vector, top_left)
+      View.new(point, multi, y_vector, x_vector, top_left, height, width, scene)
     end
 
 
@@ -29,6 +34,21 @@ module Rubytracer
       dx = @x_vector * (col + 0.5)
       point = @top_left + dy + dx
       Geom3d::Ray.new(@point, point - @point)
+    end
+
+    def colour_of_row(row)
+      (0...@width).map { |col| colour_of_pixel(row, col) }
+    end
+
+    def colour_of_pixel(row, col)
+      obj, t = @scene.intersect eye_ray(row, col)
+      if t == Float::INFINITY
+        Colour.new(0.6,0.6,0.6)
+      else
+        pos = ray.pos(t)
+        normal = obj.normal(pos)
+        obj.material.lit_colour(@scene, normal, -ray.dir, pos)
+      end
     end
 
     def to_hash 
